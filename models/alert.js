3b2ae5b50af5555866395feb87a75ffdde9b4a26
arussell36 <andrewrussell036@gmail.com>
2020-02-27 16:16:04 -0600

    restructured seed, model, and controller to make the seed work

--- models/alert.js
+++ models/alert.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const alertSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: false },
+  transit: { type: String, required: true },
   synopsis: { type: String, required: false },
   date: { type: Date, required: false }
 });
