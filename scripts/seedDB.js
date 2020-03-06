9daf989f4d5a40505eb8a02806d99d5fcb578963
Scott Kumor <scott.kumor1212@gmail.com>
2020-03-05 20:29:46 -0600

    dates

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const moment = require("moment");
 
 // This file empties the Alert collection and inserts the alerts below
 
@@ -14,11 +15,12 @@ const alertSeed =
 [
   {
     line: "Red",
-    category: "indExp",
+    category: "Indecent Exposure",
     description: "I saw an ass crack at Harrison",
     votes: -4,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Blue",
@@ -26,7 +28,8 @@ const alertSeed =
     description: "I'm like 99% I just saw him at Diversey",
     votes: 122,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Red",
@@ -34,7 +37,8 @@ const alertSeed =
     description: "We've been standing at between Thorndale and Granville for 10 mins",
     votes: 10,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Blue",
@@ -42,7 +46,8 @@ const alertSeed =
     description: "Man in faded Bulls Jacket breathed down my next al the way to Rosemont",
     votes: 1,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Purple Express",
@@ -50,7 +55,8 @@ const alertSeed =
     description: "This woman exposed her ankles on the train",
     votes: -13,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Green",
@@ -58,7 +64,8 @@ const alertSeed =
     description: "They tazed an angry Russian man on the steps Morgan",
     votes: 43,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Green",
@@ -66,7 +73,8 @@ const alertSeed =
     description: "This lady asked me if I wanted to 'dig her 'well' outside of Pulaski... yikes. Wearing pink sweatpants with 'JUICY' on the butt",
     votes: 2,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Brown",
@@ -74,7 +82,8 @@ const alertSeed =
     description: "I was asked by a guy at Southport if I liked the way he smelled!! What!",
     votes: 7,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Brown",
@@ -82,7 +91,8 @@ const alertSeed =
     description: "A guy at Kimball threatened to stab me if I didn't give him my orange mocha frappacino",
     votes: 17,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Brown",
@@ -90,7 +100,8 @@ const alertSeed =
     description: "Gasoline fight at Kimball",
     votes: 69,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Yellow",
@@ -98,7 +109,8 @@ const alertSeed =
     description: "Massive bank heist occured and they hopped on the train. Police is frisking everyone >:[",
     votes: 20,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Yellow",
@@ -106,7 +118,8 @@ const alertSeed =
     description: "Someone got trampled at the Howard stop",
     votes: 15,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Yellow",
@@ -114,7 +127,8 @@ const alertSeed =
     description: "At the skokie stop and trains like 20 mins late :/",
     votes: 2,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Pink",
@@ -122,7 +136,8 @@ const alertSeed =
     description: "got the bird from a homeless guy at the kedzie stop",
     votes: -5,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Purple Express",
@@ -130,7 +145,8 @@ const alertSeed =
     description: "bunch of high schoolers cat calling while going to the beach",
     votes: 5,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Purple Express",
@@ -138,7 +154,8 @@ const alertSeed =
     description: "classic purple line completely packed during rush hour. smells like pits out here.",
     votes: 52,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Purple",
@@ -146,7 +163,8 @@ const alertSeed =
     description: "some kid took their parents phone and is going around taking pictures of everyone.",
     votes: -2,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Purple",
@@ -154,7 +172,8 @@ const alertSeed =
     description: "middle of the afternoon and a homeless guy is blackout hitting on everyone in this train.",
     votes: 22,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Orange",
@@ -162,7 +181,8 @@ const alertSeed =
     description: "kids these day on the way to their beach are showing wayy too much ankle.",
     votes: 1,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   },
   {
     line: "Orange",
@@ -170,7 +190,8 @@ const alertSeed =
     description: "not sure whats going down at midway but the all trains to midway have stopped.",
     votes: 22,
     cleared: 0,
-    date: new Date(Date.now())
+    dateTime: moment().format("YYYYMMDDHHmmss")
+
   }
 ]
 
