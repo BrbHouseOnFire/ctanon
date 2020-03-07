d78d96e61d63e768c416b4a29a896967dd08e2ab
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-03-07 13:46:12 -0600

    functioning api loading animations

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,10 +4,10 @@ import "./style.css";
 function Nav() {
   return (
     <nav className="d-flex navbar mb-5 sticky-top justify-content-between navbar-expand-lg navbar-dark bg-dark">
-      <a id="homeBtn" className="buttonEffect" href="/"> uCTAnon </a>
-      <div className="p-2 text-primary">Unofficial CTA Anonymous Reporting</div>
+      <a id="homeBtn" className="buttonEffect" href="/">uCTAnon Alerts</a>
+      {/* <div className="p-2 text-primary">Unofficial CTA Anonymous Reporting</div> */}
       <a id="subBtn" className="buttonEffect" href="/submit">Submit Alert</a>
-      <a id="ctaBtn" className="buttonEffect" href="/ctaAlerts">CTA Alerts</a>
+      <a id="ctaBtn" className="buttonEffect" href="/ctaAlerts">Official CTA Alerts</a>
       <a id="copBtn" className="buttonEffect buttonEffect911" href="tel:911">CALL 911</a>
     </nav>
   );
