2f44225f8d8be80bfa76c776cf6515aaab84d14f
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-03-10 20:31:44 -0500

    html loader now prevents everything other than nav from displaying

--- client/src/pages/Alerts.js
+++ client/src/pages/Alerts.js
@@ -174,7 +174,6 @@ function Alerts() {
     };
   }
 
-
   // // Deletes a alert from the database with a given id, then reloads alerts from the db
   // function deleteAlert(id) {
   //   API.deleteAlert(id)
@@ -182,125 +181,92 @@ function Alerts() {
   //     .catch(err => console.log(err));
   // }
 
-
-  // // When the form is submitted, use the API.saveBook method to save the book data
-  // // Then reload books from the database
-  // function handleFormSubmit(event) {
-  //   // console.log(formObject.line.value)
-  //   console.log("form click works!!");
-  //   console.log(line);
-  //   console.log(category);
-  //   console.log(desc);
-
-  //   event.preventDefault();
-  //   //if (formObject.color && formObject.category) { i took this out to prevent requiring data -DDD
-  //   API.saveAlert({
-  //     line: line,
-  //     category: category,
-  //     description: desc,
-  //     votes: 0,
-  //     date: 'hi'
-  //   })
-  //     .then(res => loadAlerts())
-  //     .catch(err => console.log(err));
-  //   //}
-  // };
-
   return (
-
-    <div className="d-flex flex-column align-items-center m-auto w w-75">
-
-      <h1 className="display-4 m-5 mb-5">Check out what's going on...</h1>
-
-      <div className="m-auto d-flex">
-        <div className="m-4 d-flex flex-column">
-          <h3 className="text-muted"><i>Filter by Line</i></h3>
-          <select className="h-auto m-auto display-4" id="lineFilter" onChange={() => filterLine(document.getElementById("lineFilter").value, document.getElementById("sort").value)}>
-            <option className="lead" id="all" value="">All Lines</option>
-            <option className="lead" id="Red" data-val="Red">Red</option>
-            <option className="lead" id="Blue" data-val="Blue">Blue</option>
-            <option className="lead" id="Brn" data-val="Brn">Brown</option>
-            <option className="lead" id="G" data-val="G">Green</option>
-            <option className="lead" id="Org" data-val="Org">Orange</option>
-            <option className="lead" id="P" data-val="P">Purple</option>
-            <option className="lead" id="Pexp" data-val="Pexp">Purple Express</option>
-            <option className="lead" id="Pink" data-val="Pink">Pink</option>
-            <option className="lead" id="Y" data-val="Y">Yellow</option>
-          </select>
-        </div>
-
-        <div className=" m-4 d-flex flex-column">
-          <h3 className=" text-muted"><i>Order By</i></h3>
-          <select className=" m-5 h-auto m-auto display-4" id="sort" onChange={() => sorting(document.getElementById("sort").value)}>
-            <option className=" lead" data-val="mostRecent">Most Recent</option>
-            <option className=" lead" data-val="hiRated">Highest Rated</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="mnw-90">
-        {alerts.length ? (
-          <List>
-
-            {filteredAlerts.map(alert => (
-
-              <ListItem className="" key={alert._id}>
-                <div className=" d-flex justify-content-start align-items-center m-1">
-
-                  <Link to={"/alerts/" + alert._id} title="See Full Alert">
-                    <div className={alert.line + " p-3 rounded"}>{'\u00A0 \u00A0 \u00A0'}</div>
-                  </Link>
-
-                  <div className=" d-flex flex-column align-items-start ">
-                    <Link to={"/alerts/" + alert._id} className="w-desc p-3 lead" title="See Full Alert">
-                      {alert.description}
-                    </Link>
-
-                    <div className=" d-flex align-items-center justify-items-center pl-3">
-                      <div >
-                        {/* Score */}
-                        {`Score: ${alert.votes}`}
+    <>
+      {alerts.length ? (
+        <>
+          <div className="d-flex flex-column align-items-center m-auto w w-75">
+            <h1 className="display-4 m-5 mb-5">Check out what's going on...</h1>
+            <div className="m-auto d-flex">
+              <div className="m-4 d-flex flex-column">
+                <h3 className="text-muted"><i>Filter by Line</i></h3>
+                <select className="h-auto m-auto display-4" id="lineFilter" onChange={() => filterLine(document.getElementById("lineFilter").value, document.getElementById("sort").value)}>
+                  <option className="lead" id="all" value="">All Lines</option>
+                  <option className="lead" id="Red" data-val="Red">Red</option>
+                  <option className="lead" id="Blue" data-val="Blue">Blue</option>
+                  <option className="lead" id="Brn" data-val="Brn">Brown</option>
+                  <option className="lead" id="G" data-val="G">Green</option>
+                  <option className="lead" id="Org" data-val="Org">Orange</option>
+                  <option className="lead" id="P" data-val="P">Purple</option>
+                  <option className="lead" id="Pexp" data-val="Pexp">Purple Express</option>
+                  <option className="lead" id="Pink" data-val="Pink">Pink</option>
+                  <option className="lead" id="Y" data-val="Y">Yellow</option>
+                </select>
+              </div>
+              <div className=" m-4 d-flex flex-column">
+                <h3 className=" text-muted"><i>Order By</i></h3>
+                <select className=" m-5 h-auto m-auto display-4" id="sort" onChange={() => sorting(document.getElementById("sort").value)}>
+                  <option className=" lead" data-val="mostRecent">Most Recent</option>
+                  <option className=" lead" data-val="hiRated">Highest Rated</option>
+                </select>
+              </div>
+            </div>
+            <div className="mnw-90">
+              <List>
+                {filteredAlerts.map(alert => (
+                  <ListItem className="" key={alert._id}>
+                    <div className=" d-flex justify-content-start align-items-center m-1">
+                      <Link to={"/alerts/" + alert._id} title="See Full Alert">
+                        <div className={alert.line + " p-3 rounded"}>{'\u00A0 \u00A0 \u00A0'}</div>
+                      </Link>
+                      <div className=" d-flex flex-column align-items-start ">
+                        <Link to={"/alerts/" + alert._id} className="w-desc p-3 lead" title="See Full Alert">
+                          {alert.description}
+                        </Link>
+                        <div className=" d-flex align-items-center justify-items-center pl-3">
+                          <div >
+                            {/* Score */}
+                            {`Score: ${alert.votes}`}
+                          </div>
+                          <div className=" ml-4 mr-4">
+                            {/* Clears */}
+                            <img alt="" src={require('./../assets/images/ctanonImages/stale.png')} title="# of Users Who Marked This Alert as Stale" />
+                            {`x${alert.cleared}`}
+                          </div>
+                          <div >
+                            {/* post time */}
+                            <img alt="" src={require('./../assets/images/ctanonImages/pst.png')} ></img>
+                            {moment(alert.dateTime, "YYYYMMDDHHmmss").fromNow()}
+                          </div>
+                        </div>
                       </div>
-
-                      <div className=" ml-4 mr-4">
-                        {/* Clears */}
-                        <img alt="" src={require('./../assets/images/ctanonImages/stale.png')} title="# of Users Who Marked This Alert as Stale" />
-                        {`x${alert.cleared}`}
+                      <div className=" ml-auto d-flex flex-column justify-content-center align-items-center">
+                        <VoteUp onClick={() => upvote(alert)} />
+                        <Clear onClick={() => clear(alert)} />
+                        <VoteDn onClick={() => downvote(alert)} />
                       </div>
-
-                      <div >
-                        {/* post time */}
-                        <img alt="" src={require('./../assets/images/ctanonImages/pst.png')} ></img>
-                        {moment(alert.dateTime, "YYYYMMDDHHmmss").fromNow()}
-                      </div>
-                      
                     </div>
-                    
-                  </div>
-                  <div className=" ml-auto d-flex flex-column justify-content-center align-items-center">
-                    
-                    <VoteUp onClick={() => upvote(alert)} />
-                    <Clear onClick={() => clear(alert)} />
-                    <VoteDn onClick={() => downvote(alert)} />
-
-                  </div>
-                </div>
-              </ListItem>
-
-            ))}
-          </List>
-        ) : (
-            <>
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="d-flex flex-column align-items-center m-auto w w-75">
+            <div className="mnw-90">
               <div className=" loader">
                 <span className=" cube"></span>
                 {/* <span className="cube"></span> */}
                 {/* <span className="cube"></span> */}
                 {/* <span className="cube"></span> */}
               </div>
-            </>
-          )}
-      </div>
-    </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 }
 
