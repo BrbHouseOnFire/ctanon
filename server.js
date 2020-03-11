616dbac92b98639adb8a93ed55ffa0be8ba19d47
Brb House on Fire <bobbywoj@sbcglobal.net>
2020-03-11 14:36:29 -0500

    heroku routing fixes 2

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const compression = require("compression");
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(logger("dev"));
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,16 +15,15 @@ app.use(compression());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // });
 }
 // Add routes, both API and view
 app.use(routes);
-app.use(logger("dev"));
 
 // Connect to the Mongo DB
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ctanondb");
